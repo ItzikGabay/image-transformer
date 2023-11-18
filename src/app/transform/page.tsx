2870b0c76fb5227f6a745d971c560d2856b4458f
Shalom I. Gabay <itzikdevio@gmail.com>
2023-11-18 13:24:16 +0200

    refactored Homepage

---
+++ src/app/transform/page.tsx
@@ -0,0 +1,27 @@
+"use client";
+import { useState, ChangeEvent, useCallback } from "react";
+import { ImageCardList } from "@components/ui/image-card";
+import { InputUpload } from "@components/ui/input-upload";
+import { handleImageUpload } from "@lib/transformer";
+import { IImage } from "@lib/transformer/zip";
+
+export default function Home() {
+  const [images, setImages] = useState<{ file: File; imageDataUrl: string }[]>(
+    [],
+  );
+
+  const onImageUpload = useCallback(
+    (event: ChangeEvent<HTMLInputElement>) =>
+      handleImageUpload(event).then((processedImages: IImage[]) =>
+        setImages(processedImages),
+      ),
+    [],
+  );
+
+  return (
+    <div>
+      <InputUpload onChange={onImageUpload} />
+      {images && <ImageCardList images={images} />}
+    </div>
+  );
+}
